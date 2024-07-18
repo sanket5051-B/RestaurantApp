fcbb9bd65b3aa6bb4126f2c8b160322177290010
Sanket Shinde <sanket.s@bridgenext.com>
2024-07-18 18:16:35 +0530

    Added Ui Files

---
+++ RestaurantAppUi/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+<router-view/>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+.signup-container {
+  max-width: 400px;
+  margin-top:79px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto;
+  width: 100px;
+}
+
+h1 {
+  text-align: center;
+}
+
+
+label {
+        margin-right: 338px;
+  font-weight: bold;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"] {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 10px;
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #45a049;
+}
+
+button:active {
+  background-color: #3e8e41;
+}
+</style>
